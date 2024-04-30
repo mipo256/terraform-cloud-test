b5449724c6c11f356e8957f283f49cc8fd2b0c30
Mikhail2048 <mikhailpolivakha@gmail.com>
2024-04-30 16:51:05 +0300

    added repository for container registry

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 resource "yandex_container_registry" "name" {
   name = "new-container-registry-from-cloud"
 }
+
+resource "yandex_container_repository" "name" {
+    name = "${yandex_container_registry.name.id}/test-repository"
+
+  lifecycle {
+    create_before_destroy = true 
+  }
+}
